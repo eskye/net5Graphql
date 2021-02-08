861c57b53378919c84645eecb8883a5316907861
eskye <sunkanmiijatuyi@gmail.com>
2021-02-08 17:35:36 +0100

    created platform model

---
+++ CommanderGQL/Models/Platform.cs
@@ -0,0 +1,16 @@
+﻿namespace CommanderGQL.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Platform
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string LicenseKey { get; set; }
+
+    }
+}
--- CommanderGQL/Startup.cs
+++ CommanderGQL/Startup.cs
@@ -10,12 +10,23 @@ using Microsoft.Extensions.Hosting;
 
 namespace CommanderGQL
 {
+    using Microsoft.Extensions.Configuration;
+
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
